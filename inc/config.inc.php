7e22647026f9edb58ed7a6e27f0d2473d02d9d41
jenavarro40 <jenavarro40@hotmail.com>
2025-06-13 21:31:31 -0700

    define config

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -1,9 +1,14 @@
 <?php
 
-// MAKE SURE TO PUT THE REQUIRED DEFINITIONS HERE
+//Javier Enrique Navarro 300368873
+
+// REQUIRED DEFINITIONS
+define("ITEM_COST",25);
+define("WRAP_COST",10);
+define("DISCOUNT",0.15);
+define("TAX",0.12);
 
 // DECLARE THE ASSOCIATIVE ARRAY SHIPPING COST
-
-
+$shippingCost = ["regular" => 6,"express" => 15, "priority" => 25]
 
 ?>
